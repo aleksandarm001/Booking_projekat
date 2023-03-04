e167fb4814691e10813ca2b329216db4423a9585
aleksandarm001 <aleksandar.marinkovic6366@gmail.com>
2023-03-04 12:47:19 +0100

    Reservation class created

---
+++ InitialProject/CustomClasses/Reservation.cs
@@ -0,0 +1,59 @@
+﻿using InitialProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.CustomClasses
+{
+    public class Reservation
+    {
+        private string accommodationID;
+        private DateTime startDate;
+        private string sStartDate;
+        private DateTime endDate;
+        private string sEndDate;
+
+        public Reservation(string accommodationID, DateTime startDate, DateTime endDate)
+        {
+            this.AccommodationID = accommodationID;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+            sStartDate = string.Format("{0:dd.MM.yyyy.}", startDate);
+            sEndDate = string.Format("{0:dd.MM.yyyy.}", endDate);
+        }
+
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+        public DateTime EndDate { get => endDate; set => endDate = value; }
+        public string AccommodationID { get => accommodationID; set => accommodationID = value; }
+
+        public override string ToString()
+        {
+            return accommodationID + ";" + startDate.ToString() + ";" + endDate.ToString();
+        }
+        public Reservation fromStringToReservation(string s)
+        {
+            string[] reservation = new string[3];
+            reservation = s.Split(';');
+            return new Reservation(reservation[0],DateTime.Parse(reservation[1]), DateTime.Parse(reservation[2]));
+        }
+        public string[] ToCSV()
+        {
+            string[] csvValues = {
+                accommodationID,
+                startDate.ToString(),
+                endDate.ToString()};
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            accommodationID = values[0];
+            startDate = DateTime.Parse(values[1]);
+            sStartDate = string.Format("{0:dd.MM.yyyy.}", startDate);
+            endDate = DateTime.Parse(values[2]); 
+            sEndDate = string.Format("{0:dd.MM.yyyy.}", endDate);
+        }
+    }
+}
--- InitialProject/Model/Accommodation.cs
+++ InitialProject/Model/Accommodation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InitialProject.CustomClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,20 +19,26 @@ namespace InitialProject.Model
         private int maxGuestNumber;
         private int minReservationDays;
         private int daysBeforeCancelling = 1;
-        private List<String> images = new List<String>();
+        private List<String> images = new List<String>(); 
+        private List<Reservation> reservations = new List<Reservation>();
 
-        public Accommodation(string naziv, Location location, Type type, int maxGuestNumber, int minReservationDays, int daysBeforeCancelling, List<String> images)
+        public Accommodation(string accommodationID, string name, Location location, Type type, int maxGuestNumber, 
+            int minReservationDays, int daysBeforeCancelling, List<String> images,
+            List<Reservation> reservations)
         {
-            this.name = naziv;
+            this.accommodationID = accommodationID;
+            this.name = name;
             this.location = location;
             this.type = type;
             this.maxGuestNumber = maxGuestNumber;
             this.minReservationDays = minReservationDays;
             this.daysBeforeCancelling = daysBeforeCancelling;
             this.images = images;
+            this.Reservations = reservations;
         }
         public Accommodation()
         {
+            this.accommodationID = "";
             this.name = "";
             this.location = new Location();
             this.type = Type.appartment;
@@ -39,6 +46,7 @@ namespace InitialProject.Model
             minReservationDays = 0;
             daysBeforeCancelling = 1;
             this.images = new List<String>();
+            this.Reservations = new List<Reservation>();
         }
 
         public string Name { get => name; set => name = value; }
@@ -48,6 +56,7 @@ namespace InitialProject.Model
         public int MinReservationDays { get => minReservationDays; set => minReservationDays = value; }
         public int DaysBeforeCancelling { get => daysBeforeCancelling; set => daysBeforeCancelling = value; }
         public List<String> Images { get => images; set => images = value; }
+        public List<Reservation> Reservations { get => reservations; set => reservations = value; }
 
         public string[] ToCSV()
         {
