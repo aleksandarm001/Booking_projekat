cc6f03adc62b628ee57bae29b5b6cc8870a16040
aleksandarm001 <aleksandar.marinkovic6366@gmail.com>
2023-03-04 13:23:01 +0100

    user class created with basic information and types

--- InitialProject/Model/User.cs
+++ InitialProject/Model/User.cs
@@ -3,31 +3,60 @@ using System;
 
 namespace InitialProject.Model
 {
+    public enum UserType {Owner = 0, Guest1, Guest2, Guide}
     public class User : ISerializable
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        private int userId;
+        private string username;
+        private string password;
+        private string email;
+        private string phoneNumber;
+        private UserType userType;
 
-        public User() { }
+        public int UserId { get => userId; set => userId = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        internal UserType UserType { get => userType; set => userType = value; }
 
-        public User(string username, string password)
+        public User(int userId, string username, 
+            string password, string email, 
+            string phoneNumber, UserType userType)
         {
-            Username = username;
-            Password = password;
+            this.userId = userId;
+            this.username = username;
+            this.password = password;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            this.userType = userType; 
+        }
+        public User()
+        {
+
         }
 
         public string[] ToCSV()
         {
-            string[] csvValues = { Id.ToString(), Username, Password };
+            string[] csvValues = {
+                userId.ToString(),
+                username,
+                password,
+                email,
+                phoneNumber,
+                userType.ToString()
+            };
             return csvValues;
         }
 
         public void FromCSV(string[] values)
         {
-            Id = Convert.ToInt32(values[0]);
-            Username = values[1];
-            Password = values[2];
+            userId = Convert.ToInt32(values[0]);
+            username = values[1];
+            password = values[2];
+            email = values[3];
+            phoneNumber = values[4];
+            userType = (UserType)Enum.Parse(typeof(UserType), values[5]);
         }
     }
 }
