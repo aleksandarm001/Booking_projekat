3f4f9352bb3fd37bf1380dad71f39fc1d00a0cb8
Antonije Erdeljan <antonijeerdeljan01@gmail.com>
2023-03-04 14:53:00 +0100

    Fixed Location and KeyPoints Datatype's

--- InitialProject/Model/Tour.cs
+++ InitialProject/Model/Tour.cs
@@ -1,43 +1,43 @@
 ﻿using InitialProject.Serializer;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 
 namespace InitialProject.Model
 {
     public class Tour : ISerializable
     {
+        public int TourId { get; set; }
         public string Name { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
         public string Description { get; set; }
         public string Language { get; set; }
         public int MaxGuestNumber { get; set; }
-        public string KeyPoints { get; set; }
+
+        public List<int> KeyPointsId { get; set; }
+        public List<Location> KeyPoints { get; set; }
         public DateTime StartingDateTimes { get; set; }
         public int Duration { get; set; }
         public List<String> Images { get; set; }
-        public int TourId { get; set; }
 
         public Tour()
         {
             Name = "";
-            Country = "";
-            City = "";
+            Location = new Location();
             Description = "";
             Language = "";
             MaxGuestNumber = 1;
-            KeyPoints = "";
+            KeyPoints = new List<Location>();
             StartingDateTimes = DateTime.Now;
             Duration = 1;
             Images = new List<String>();
         }
 
-        public Tour(string name, string country, string city, string description, string language, int maxGuestNumber, string keyPoints, DateTime startingDateTimes, int duration, List<String> images)
+        public Tour(string name, Location location, string description, string language, int maxGuestNumber, List<Location> keyPoints, DateTime startingDateTimes, int duration, List<String> images)
         {
             Name = name;
-            Country = country;
-            City = city;
+            Location = location;
             Description = description;
             Language = language;
             MaxGuestNumber = maxGuestNumber;
@@ -53,21 +53,25 @@ namespace InitialProject.Model
         {
             TourId = Convert.ToInt32(values[0]);
             Name = values[1];
-            Country = values[2];
-            City = values[3];
+            LocationId = Convert.ToInt32(values[2]);
             Description = values[4];
             Language = values[5];
             MaxGuestNumber = Convert.ToInt32(values[6]);
-            KeyPoints = values[7];
+
+            KeyPointsId = values[7].Split(";").Select(int.Parse).ToList();
+
             StartingDateTimes = Convert.ToDateTime(values[8]);
             Duration = Convert.ToInt32(values[9]);
-            //Images = values[10]; 
+            Images = values[10].Split(";").ToList<string>();
 
         }
 
+
+
+
         public string[] ToCSV()
         {
-            string[] csvValues = { TourId.ToString(), Name, Country, City, Description, Language, MaxGuestNumber.ToString(), KeyPoints, StartingDateTimes.ToString(), Duration.ToString(), Images.ToString() };
+            string[] csvValues = { TourId.ToString(), Name, Location.ToString(), Description, Language, MaxGuestNumber.ToString(), KeyPoints.ToString(), StartingDateTimes.ToString(), Duration.ToString(), Images.ToString() };
             return csvValues;
         }
     }
