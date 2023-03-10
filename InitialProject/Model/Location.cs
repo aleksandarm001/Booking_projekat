b42e36134aa4b350919ef464c5b2654a23b84a52
Ljuban Cickovic <ljuban.cickovic@gmail.com>
2023-03-10 18:49:06 +0100

    Some changes

--- InitialProject/Model/Location.cs
+++ InitialProject/Model/Location.cs
@@ -11,8 +11,9 @@ namespace InitialProject.Model
     public class Location : ISerializable
     {
         public int Id { get; set;}
-        public string City { get; set; }
         public string Country { get; set; }
+        public string City { get; set; }
+        
 
         public Location(string city, string country)
         {
@@ -44,8 +45,8 @@ namespace InitialProject.Model
         {
             string[] csvValues = {
                 Id.ToString(),
-                Country,
-                City};
+                City,
+                Country};
             
             return csvValues;
         }
@@ -53,8 +54,8 @@ namespace InitialProject.Model
         public void FromCSV(string[] values)
         {
             Id = Convert.ToInt32(values[0]);
-            Country = values[1];
-            City = values[2];
+            City = values[1];
+            Country = values[2];
         }
     }
 }
