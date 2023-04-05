e665d10a3d9ee8b8f8c39fe66ad5ae69d2dcb436
aleksandarm001 <aleksandar.marinkovic6366@gmail.com>
2023-04-05 18:09:06 +0200

    StartedChangeReservation

--- InitialProject/Services/ChangeReservationRequestService.cs
+++ InitialProject/Services/ChangeReservationRequestService.cs
@@ -11,11 +11,21 @@ namespace InitialProject.Services
     public class ChangeReservationRequestService
     {
         private readonly ChangeReservationRequestRepository _requestRepository;
+        private readonly ReservationService _reservationService;
+        public ChangeReservationRequestService()
+        {
+            _requestRepository = new ChangeReservationRequestRepository();
+            _reservationService = new ReservationService();
+        }
 
         public List<ChangeReservationRequest> getRequests()
         {
             return _requestRepository.GetAll();
         }
 
+        public void SaveRequest(ChangeReservationRequest request)
+        {
+            _requestRepository.Save(request);
+        }
     }
 }
---
+++ InitialProject/Services/ReservationService.cs
@@ -0,0 +1,39 @@
+﻿using InitialProject.CustomClasses;
+using InitialProject.Repository;
+using InitialProject.View.Guest1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Services
+{
+    public class ReservationService
+    {
+        private readonly ReservationRepository _repository;
+        private readonly AccommodationService _accommodationService;
+        public ReservationService()
+        {
+            _repository = new ReservationRepository();
+            _accommodationService = new AccommodationService();
+        }
+
+        private List<Reservation> GetReservationsByUserId(int userId)
+        {
+            return _repository.GetAll().Where(r => r.UserId == userId).ToList();
+        }
+        public Dictionary<int, string> ReservationsForChange(int userId)
+        {
+            Dictionary<int, string> result = new Dictionary<int, string>();
+            List<Reservation> usersReservations = GetReservationsByUserId(userId);
+            foreach(Reservation reservation in usersReservations)
+            {
+                int accommodationId = _accommodationService.GetAccommodationIdByReservationId(reservation.ReservationId);
+                string accommodationName = _accommodationService.getNameById(accommodationId);
+                result.Add(reservation.ReservationId, accommodationName);
+            }
+            return result;
+        }
+    }
+}
