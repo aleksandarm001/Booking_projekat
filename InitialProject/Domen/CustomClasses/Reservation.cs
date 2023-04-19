f988589fa6ca14766037850ebcc30100c7abb20f
aleksandarm001 <aleksandar.marinkovic6366@gmail.com>
2023-04-19 19:12:57 +0200

    FixedBugs

--- InitialProject/Domen/CustomClasses/Reservation.cs
+++ InitialProject/Domen/CustomClasses/Reservation.cs
@@ -6,37 +6,29 @@ namespace InitialProject.CustomClasses
     public class Reservation : ISerializable
     {
         public int ReservationId { get; set; }
-        
-        public int TourId { get; set; }
         public int UserId { get; set; }
         public float AvgRating { get; set; }
         public DateRange ReservationDateRange { get; set; }
         public int NumberOfGuests { get; set; }
-        public int VoucherId { get; set; }
 
         public Reservation() 
         {
             UserId = -1;
-            TourId = -1;
             AvgRating = 0;
             ReservationDateRange = new DateRange();
             NumberOfGuests = 0;
-            VoucherId = 0;
         }
 
-        public Reservation(int userId, int tourId, DateTime startDate, int numberOfVisitors, int voucherId)
+        public Reservation(int userId,  DateTime startDate, int numberOfVisitors)
         {
             UserId = userId;
-            TourId = tourId;
             ReservationDateRange = new DateRange(startDate, numberOfVisitors);  
             NumberOfGuests = numberOfVisitors;
             AvgRating = 0;
-            VoucherId = voucherId;
         }
 
         public Reservation(DateRange dateRange, int guestNumber, int userId)
         {
-            TourId = -1;
             UserId = userId;
             ReservationDateRange = dateRange;
             NumberOfGuests = guestNumber;
@@ -46,13 +38,10 @@ namespace InitialProject.CustomClasses
         {
             string[] csvValues = {
                 ReservationId.ToString(),
-                TourId.ToString(),
-                AvgRating.ToString(),
-                NumberOfGuests.ToString(),
                 ReservationDateRange.ToString(),
+                NumberOfGuests.ToString(),
                 UserId.ToString(),
-                VoucherId.ToString(),
-                
+                AvgRating.ToString(),
             };
                 
             return csvValues;
@@ -60,14 +49,11 @@ namespace InitialProject.CustomClasses
 
         public void FromCSV(string[] values)
         {
-            ReservationId = Convert.ToInt32(values[0]);   
-            TourId = Convert.ToInt32(values[1]);
-            AvgRating = float.Parse(values[2]);
-            NumberOfGuests = Convert.ToInt32(values[3]);
-            ReservationDateRange = ReservationDateRange.fromStringToDateRange(values[4]);
-            UserId = Convert.ToInt32(values[5]);
-            VoucherId = Convert.ToInt32(values[6]);
-           
+            ReservationId = Convert.ToInt32(values[0]);
+            ReservationDateRange = ReservationDateRange.fromStringToDateRange(values[1]);
+            NumberOfGuests = Convert.ToInt32(values[2]);
+            UserId = Convert.ToInt32(values[3]);
+            AvgRating = float.Parse(values[4]);
         }
 
     }
