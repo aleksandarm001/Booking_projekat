b112103dc3d601bd82a7d3c744dc0d2972f643e5
Antonije Erdeljan <antonijeerdeljan01@gmail.com>
2023-05-04 17:31:19 +0200

    Added HomeScreen

---
+++ InitialProject/Presentation/WPF/View/Guide/GuideHomeScreen.xaml.cs
@@ -0,0 +1,39 @@
+﻿using InitialProject.View;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace InitialProject.Presentation.WPF.View.Guide
+{
+    /// <summary>
+    /// Interaction logic for GuideHomeScreen.xaml
+    /// </summary>
+    public partial class GuideHomeScreen : Window
+    {
+        public GuideHomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateTourButton(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void BurgerMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
