2f5da8d5811b2cd1934fa9ca37c14f93387ff06b
aleksandarm001 <aleksandar.marinkovic6366@gmail.com>
2023-05-07 21:27:56 +0200

    Started Validation

---
+++ InitialProject/Presentation/WPF/ViewModel/Validation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace InitialProject.Presentation.WPF.ViewModel
+{
+    public class NumericValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                var s = value as string;
+                int r;
+                if (int.TryParse(s, out r))
+                {
+                    return new ValidationResult(true, null);
+                }
+                return new ValidationResult(false, "Please enter a valid number.");
+            }
+            catch
+            {
+                return new ValidationResult(false, "Unknown error occured.");
+            }
+        }
+    }
+}
+
