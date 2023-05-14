650ddf5b6ef54080f34f30d8cf4070a7f97f2c3e
Ljuban Cickovic <ljuban.cickovic@gmail.com>
2023-05-14 23:26:08 +0200

    Fixed some bugs

---
+++ InitialProject/Presentation/WPF/View/Guest2/TourNotification.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace InitialProject.Presentation.WPF.View.Guest2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for TourNotification.xaml
+    /// </summary>
+    public partial class TourNotification : Window
+    {
+        public TourNotification()
+        {
+            InitializeComponent();
+        }
+
+        private void Odustani_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Potvrdi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
